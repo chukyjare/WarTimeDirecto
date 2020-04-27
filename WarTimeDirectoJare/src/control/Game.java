d102d16a4d23780b6b681827b315ebccd3a1358d
ChukyJare <ChukyJare@Chukyjare>
2020-04-27 19:25:24 +0200

    Profesor tarea

---
+++ WarTimeDirectoJare/src/control/Game.java
@@ -0,0 +1,15 @@
+package control;
+
+import model.Board;
+import model.Dimensionn;
+
+public class Game {
+	private Board board;
+
+	public Game(Dimensionn dimensionn) {
+		super();
+		board= new Board(dimensionn);
+	}
+	
+	
+}
